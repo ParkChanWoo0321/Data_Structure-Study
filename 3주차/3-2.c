5a1c7a34182f706d836ffb746365f404e2da3062
ChanWoo <op9563_@naver.com>
2026-02-15 12:15:55 +0900

    Update 3-2.c

--- 3주차/3-2.c
+++ 3주차/3-2.c
@@ -1,79 +1,79 @@
-//// 2. Èñ¼ÒÇà·ÄÀÇ ºü¸¥ ÀüÄ¡ ( ±³Àç ÇÁ·Î±×·¥ 2.9 )
-//#include <stdio.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#define MAX_TERMS 101
-//#define MAX_COL 101
-//typedef struct
-//{
-//	int col;
-//	int row;
-//	int value;
-//}term;
-//term a[MAX_TERMS];
-//term b[MAX_TERMS];
-//void fastTranspose(term a[],term b[])
-//{
-//	int rowTerms[MAX_COL], startingPos[MAX_COL];
-//	int i, j, numCols = a[0].col, numTerms = a[0].value;
-//	b[0].row = numCols; b[0].col = a[0].row;
-//	b[0].value = numTerms;
-//	if (numTerms > 0)
-//	{
-//		for (i = 0; i < numCols; i++)
-//			rowTerms[i] = 0;
-//		for (i = 1; i <= numTerms; i++)
-//			rowTerms[a[i].col]++;
-//		startingPos[0] = 1;
-//		for (i = 1; i < numCols; i++)
-//			startingPos[i] = startingPos[i - 1] + rowTerms[i - 1];
-//		for (i = 1; i <= numTerms; i++)
-//		{
-//			j = startingPos[a[i].col]++;
-//			b[j].row = a[i].col; b[j].col = a[i].row;
-//			b[j].value = a[i].value;
-//		}
-//	}
-//}
-//int main(void)
-//{
-//	int i;
-//	printf("ÇàÀÇ °³¼ö : ");
-//	scanf_s("%d", &a[0].row);
-//	printf("¿­ÀÇ °³¼ö : ");
-//	scanf_s("%d", &a[0].col);
-//	printf("°ªÀÇ °³¼ö : ");
-//	scanf_s("%d", &a[0].value);
-//	if (a[0].value >= MAX_TERMS)
-//	{
-//		fprintf(stderr, "°ªÀÇ °³¼ö°¡ ³Ê¹« Å®´Ï´Ù.\n");
-//		exit(EXIT_FAILURE);
-//	}
-//	else
-//	{
-//		for (i = 1; i <= a[0].value; i++)
-//		{
-//			printf("\nÇà : ");
-//			scanf_s("%d", &a[i].row);
-//			printf("¿­ : ");
-//			scanf_s("%d", &a[i].col);
-//			if (a[i].row >= a[0].row
-//				|| a[i].col >= a[0].col)
-//			{
-//				fprintf(stderr, "ÀÔ·ÂÇÑ Çà ¶Ç´Â ¿­ÀÌ \
-//			%d * %d º¸´Ù Å®´Ï´Ù.\n", a[0].row, a[0].col);
-//				exit(EXIT_FAILURE);
-//			}
-//			printf("°ª : ");
-//			scanf_s("%d", &a[i].value);
-//		}
-//	}
-//	fastTranspose(a, b);
-//	printf("\n\tA\t\t\tB\n");
-//	printf("\tÇà\t¿­\t°ª\t\tÇà\t¿­\t°ª\n");
-//	for (i = 0; i <= a[0].value; i++)
-//	{
-//		printf("a[%d]\t%2d\t%2d\t%2d\t", i, a[i].row, a[i].col, a[i].value);
-//		printf("b[%d]\t%2d\t%2d\t%2d\n", i, b[i].row, b[i].col, b[i].value);
-//	}
-//}
+// 2. í¬ì†Œí–‰ë ¬ì˜ ë¹ ë¥¸ ì „ì¹˜ ( êµì¬ í”„ë¡œê·¸ë¨ 2.9 )
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_TERMS 101
+#define MAX_COL 101
+typedef struct
+{
+	int col;
+	int row;
+	int value;
+}term;
+term a[MAX_TERMS];
+term b[MAX_TERMS];
+void fastTranspose(term a[],term b[])
+{
+	int rowTerms[MAX_COL], startingPos[MAX_COL];
+	int i, j, numCols = a[0].col, numTerms = a[0].value;
+	b[0].row = numCols; b[0].col = a[0].row;
+	b[0].value = numTerms;
+	if (numTerms > 0)
+	{
+		for (i = 0; i < numCols; i++)
+			rowTerms[i] = 0;
+		for (i = 1; i <= numTerms; i++)
+			rowTerms[a[i].col]++;
+		startingPos[0] = 1;
+		for (i = 1; i < numCols; i++)
+			startingPos[i] = startingPos[i - 1] + rowTerms[i - 1];
+		for (i = 1; i <= numTerms; i++)
+		{
+			j = startingPos[a[i].col]++;
+			b[j].row = a[i].col; b[j].col = a[i].row;
+			b[j].value = a[i].value;
+		}
+	}
+}
+int main(void)
+{
+	int i;
+	printf("í–‰ì˜ ê°œìˆ˜ : ");
+	scanf_s("%d", &a[0].row);
+	printf("ì—´ì˜ ê°œìˆ˜ : ");
+	scanf_s("%d", &a[0].col);
+	printf("ê°’ì˜ ê°œìˆ˜ : ");
+	scanf_s("%d", &a[0].value);
+	if (a[0].value >= MAX_TERMS)
+	{
+		fprintf(stderr, "ê°’ì˜ ê°œìˆ˜ê°€ ë„ˆë¬´ í½ë‹ˆë‹¤.\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		for (i = 1; i <= a[0].value; i++)
+		{
+			printf("\ní–‰ : ");
+			scanf_s("%d", &a[i].row);
+			printf("ì—´ : ");
+			scanf_s("%d", &a[i].col);
+			if (a[i].row >= a[0].row
+				|| a[i].col >= a[0].col)
+			{
+				fprintf(stderr, "ì…ë ¥í•œ í–‰ ë˜ëŠ” ì—´ì´ \
+			%d * %d ë³´ë‹¤ í½ë‹ˆë‹¤.\n", a[0].row, a[0].col);
+				exit(EXIT_FAILURE);
+			}
+			printf("ê°’ : ");
+			scanf_s("%d", &a[i].value);
+		}
+	}
+	fastTranspose(a, b);
+	printf("\n\tA\t\t\tB\n");
+	printf("\tí–‰\tì—´\tê°’\t\tí–‰\tì—´\tê°’\n");
+	for (i = 0; i <= a[0].value; i++)
+	{
+		printf("a[%d]\t%2d\t%2d\t%2d\t", i, a[i].row, a[i].col, a[i].value);
+		printf("b[%d]\t%2d\t%2d\t%2d\n", i, b[i].row, b[i].col, b[i].value);
+	}
+}
