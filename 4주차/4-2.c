f7bf29406024ddd737a433761389f9dceffe00e0
ChanWoo <op9563_@naver.com>
2026-03-07 16:05:03 +0900

    Update 4-2.c

--- 4주차/4-2.c
+++ 4주차/4-2.c
@@ -1,53 +1,53 @@
-// 2. Èü ¼ÒÆ® ±¸Çö(±³Àç ÇÁ·Î±×·¥ 7.12, 7.13)
-//#include <stdio.h>
-//#define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t)) 
-//typedef struct {
-//	int key;
-//} element;
-//void adjust(element a[], int root, int n) 
-//{
-//	int child, rootkey;
-//	element temp;
-//	temp = a[root];
-//	rootkey = a[root].key;
-//	child = 2 * root;
-//	while (child <= n) {
-//		if ((child < n) && (a[child].key < a[child + 1].key))
-//			child++;
-//		if (rootkey > a[child].key)
-//			break;
-//		else {
-//			a[child / 2] = a[child];
-//			child *= 2;
-//		}
-//	}
-//	a[child / 2] = temp;
-//}
-//void heapSort(element a[], int n) 
-//{
-//	int i;
-//	element temp;
-//	for (i = n / 2; i > 0; i--)
-//		adjust(a, i, n);
-//	for (i = n - 1; i > 0; i--) {
-//		SWAP(a[1], a[i + 1], temp);
-//		adjust(a, 1, i);
-//	}
-//}
-//int main(void) {
-//	element rawList[] = 
-//	{ {26}, {5}, {77}, {1}, {61}, {11}, {59}, {15}, {48}, {19} };
-//	int size = sizeof(rawList) / sizeof(rawList[0]);
-//	element list[11];
-//	for (int i = 0; i < size; i++)
-//		list[i + 1] = rawList[i];
-//	printf("Á¤·Ä Àü: ");
-//	for (int i = 1; i <= size; i++)
-//		printf("%d ", list[i].key);
-//	printf("\n");
-//	heapSort(list, size);
-//	printf("Á¤·Ä ÈÄ: ");
-//	for (int i = 1; i <= size; i++)
-//		printf("%d ", list[i].key);
-//	printf("\n");
-//}
+ 2. íž™ ì†ŒíŠ¸ êµ¬í˜„(êµìž¬ í”„ë¡œê·¸ëž¨ 7.12, 7.13)
+#include <stdio.h>
+#define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t)) 
+typedef struct {
+	int key;
+} element;
+void adjust(element a[], int root, int n) 
+{
+	int child, rootkey;
+	element temp;
+	temp = a[root];
+	rootkey = a[root].key;
+	child = 2 * root;
+	while (child <= n) {
+		if ((child < n) && (a[child].key < a[child + 1].key))
+			child++;
+		if (rootkey > a[child].key)
+			break;
+		else {
+			a[child / 2] = a[child];
+			child *= 2;
+		}
+	}
+	a[child / 2] = temp;
+}
+void heapSort(element a[], int n) 
+{
+	int i;
+	element temp;
+	for (i = n / 2; i > 0; i--)
+		adjust(a, i, n);
+	for (i = n - 1; i > 0; i--) {
+		SWAP(a[1], a[i + 1], temp);
+		adjust(a, 1, i);
+	}
+}
+int main(void) {
+	element rawList[] = 
+	{ {26}, {5}, {77}, {1}, {61}, {11}, {59}, {15}, {48}, {19} };
+	int size = sizeof(rawList) / sizeof(rawList[0]);
+	element list[11];
+	for (int i = 0; i < size; i++)
+		list[i + 1] = rawList[i];
+	printf("ì •ë ¬ ì „: ");
+	for (int i = 1; i <= size; i++)
+		printf("%d ", list[i].key);
+	printf("\n");
+	heapSort(list, size);
+	printf("ì •ë ¬ í›„: ");
+	for (int i = 1; i <= size; i++)
+		printf("%d ", list[i].key);
+	printf("\n");
+}
